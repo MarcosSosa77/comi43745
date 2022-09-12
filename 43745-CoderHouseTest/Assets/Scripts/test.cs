17a87d9a12cef294619619c4698d3a6f64e2f028
MarcosSosa77 <marquitos.sosa69@gmail.com>
2022-09-12 17:44:38 -0300

    v 0.0.1
    (Versión inicial del proyecto - Escena de Unity Vacia)

---
+++ 43745-CoderHouseTest/Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class test: MonoBehaviour
+{
+   
+    void Start()
+    {
+	Saludar();        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Saludar()
+    {
+	Debug.Log("Holaaa");
+    }
+}
